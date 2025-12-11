b752db2bf19a6ee4893ece443eeaa04c3a3d53b6
fishwww-ww <2470335462@qq.com>
2025-12-11 15:47:56 +0800

    fix: wrong download url

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("badasstechie/celebahq-resized-256x256")
+path = kagglehub.dataset_download("sachchitkunichetty/rvf10k")
 
 print("Path to dataset files:", path)
